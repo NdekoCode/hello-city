9d10ef521680caa7f70fb87ea34dde627f8db290
Arick Bulakali <arickbulakali@gmail.com>
2022-04-24 13:14:07 +0200

    Make use Route::view, Route with arrow function, Route with full manner

--- routes/web.php
+++ routes/web.php
@@ -12,14 +12,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Les trois facons de faires des routes sans controller
 
+// 01. De facon complet et complexe
 Route::get('/', function () {
     return view('pages.home');
 })->name('app_home');
-Route::get('/about-us', function () {
-    return view('pages.about');
-})->name('app_about');
 
-Route::get('/help', function () {
-    return view('pages.help');
-})->name('help');
+// 02. Avec des fonction flechés
+Route::get('/about-us', fn () => view('pages.about'))->name('app_about');
+
+// Avec la methode static view
+Route::view('/help', 'pages.help')->name('help');
